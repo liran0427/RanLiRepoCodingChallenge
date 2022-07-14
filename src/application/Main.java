67f0cd28526f5fa763d846fb48af5d3d24041672
saja.abufarha <saja.abufarha@IC-CS233-24.uc.ucalgary.ca>
2022-07-14 16:38:13 -0600

    Changed the variable root to VBox, and the title text to Saja's project

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,11 +10,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane VBox = new BorderPane();
+			Scene scene = new Scene(VBox,400,400);
 //			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Ran's project");
+			primaryStage.setTitle("Saja's project");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
