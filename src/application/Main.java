c8d4a7dcaf713fb6fa57bde1f2ee9a06784c7018
li23 <li23@IC-CS233-5.uc.ucalgary.ca>
2022-07-14 17:25:57 -0600

    added my master branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			VBox root = new VBox();
+			root.getChildren().add(new Label("Ran's master branch"));
 			root.getChildren().add(new Label("Saja's label"));
 			Scene scene = new Scene(root,400,400);
 //			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
