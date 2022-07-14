19526e797cd9431eb1d283c9ce04e1393b1aaef9
saja.abufarha <saja.abufarha@IC-CS233-24.uc.ucalgary.ca>
2022-07-14 16:49:33 -0600

    Changed variables name root to VBox and title text

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane VBox = new BorderPane();
-			Scene scene = new Scene(VBox,400,400);
+			VBox root = new VBox();
+			root.getChildren().add(new Label("Saja's label"));
+			Scene scene = new Scene(root,400,400);
 //			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Saja's project");
+			primaryStage.setTitle("Ran's project");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
